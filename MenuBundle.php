cddc7fc0343437699f250d83cd190e4c0021c8fd
ornicar <thibault.duplessis@gmail.com>
2010-08-15 14:48:53 +0200

    Cleanup MenuBundle class

--- MenuBundle.php
+++ MenuBundle.php
@@ -2,17 +2,8 @@
 
 namespace Bundle\MenuBundle;
 
-use Bundle\MenuBundle\DependencyInjection\MenuExtension;
 use Symfony\Framework\Bundle\Bundle as BaseBundle;
-use Symfony\Components\DependencyInjection\ParameterBag\ParameterBagInterface;
-use Symfony\Components\DependencyInjection\ContainerBuilder;
 
 class MenuBundle extends BaseBundle
 {
-
-    public function buildContainer(ParameterBagInterface $parameterBag)
-    {
-        ContainerBuilder::registerExtension(new MenuExtension());
-    }
-
 }
