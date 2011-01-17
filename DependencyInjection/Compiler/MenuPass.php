120060d5ca903be57123d517e9feab2c2584bcf1
Jimmy Leger <jimmy.leger@gmail.com>
2011-01-17 16:59:00 +0100

    added a compiler pass to retrieve all menu classes

---
+++ DependencyInjection/Compiler/MenuPass.php
@@ -0,0 +1,33 @@
+<?php
+namespace Bundle\MenuBundle\DependencyInjection\Compiler;
+
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
+
+class MenuPass implements CompilerPassInterface
+{
+    public function process(ContainerBuilder $container)
+    {
+	if ($container->hasDefinition('twig')) {
+	        if (!$container->hasDefinition('twig.extension.menu')) {
+	            return;
+	        }
+	}
+
+	if ($container->hasDefinition('templating')) {
+		if (!$container->hasDefinition('templating.helper.menu')) {
+	            return;
+	        }
+	}
+
+        $menus = array();
+
+	foreach ($container->findTaggedServiceIds('menu') as $id => $attributes) {
+            if (isset($attributes[0]['alias'])) {
+                $menus[$attributes[0]['alias']] = $id;
+            }
+        }
+
+        $container->setParameter('menu.classes', $menus);
+    }
+}
