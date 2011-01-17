f65ba94f7d87d655e86c26d9214bc33b2e960dc5
Jimmy Leger <jimmy.leger@gmail.com>
2011-01-17 17:07:53 +0100

    fix indentation

--- DependencyInjection/Compiler/MenuPass.php
+++ DependencyInjection/Compiler/MenuPass.php
@@ -8,26 +8,26 @@ class MenuPass implements CompilerPassInterface
 {
     public function process(ContainerBuilder $container)
     {
-	if ($container->hasDefinition('twig')) {
-	        if (!$container->hasDefinition('twig.extension.menu')) {
-	            return;
-	        }
-	}
+        if ($container->hasDefinition('twig')) {
+            if (!$container->hasDefinition('twig.extension.menu')) {
+                return;
+            }
+        }
 
-	if ($container->hasDefinition('templating')) {
-		if (!$container->hasDefinition('templating.helper.menu')) {
-	            return;
-	        }
-	}
+        if ($container->hasDefinition('templating')) {
+            if (!$container->hasDefinition('templating.helper.menu')) {
+                return;
+            }
+        }
 
         $menus = array();
 
-	foreach ($container->findTaggedServiceIds('menu') as $id => $attributes) {
+        foreach ($container->findTaggedServiceIds('menu') as $id => $attributes) {
             if (isset($attributes[0]['alias'])) {
                 $menus[$attributes[0]['alias']] = $id;
             }
         }
 
-        $container->setParameter('menu.classes', $menus);
+        $container->setParameter('menu.services', $menus);
     }
 }
