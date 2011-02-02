3d212cd496f5e06abc9c69ef8557d0f8842fcd21
Florian Klein <florian.klein@knplabs.com>
2011-02-02 14:01:28 +0100

    had to call getName in templates as MenuItem::__toString has changed

--- Resources/views/Menu/item.html.php
+++ Resources/views/Menu/item.html.php
@@ -1,6 +1,6 @@
 <?php if($item->shouldBeRendered()): ?>
     <li <?php echo $view['menu']->attributes($view['menu']->getItemAttributes($item)) ?>>
-        <a href="<?php echo $item->getUri() ?>"><?php echo $item ?></a>
+        <a href="<?php echo $item->getUri() ?>"><?php echo $item->getName() ?></a>
     </li>
     <?php if($item->hasChildren()): ?>
         <?php echo $view->render('MenuBundle:Menu:menu.html.php', array('item' => $item)) ?>
