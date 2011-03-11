a5ce632495959ac7b0d40022c66a50193759e940
Kris Wallsmith <kris.wallsmith@gmail.com>
2011-03-11 09:24:55 -0800

    fixed reference to removed class

--- DependencyInjection/KnplabsMenuExtension.php
+++ DependencyInjection/KnplabsMenuExtension.php
@@ -2,7 +2,7 @@
 
 namespace Knplabs\MenuBundle\DependencyInjection;
 
-use Symfony\Component\DependencyInjection\Extension\Extension;
+use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 use Symfony\Component\DependencyInjection\Loader\XmlFileLoader;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\Config\FileLocator;
