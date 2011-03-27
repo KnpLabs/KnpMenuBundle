ff85033f7468fdec0bf0ecd6228315bb88017501
Sorin <sorin.neacsu@gmail.com>
2011-03-27 02:26:08 -0700

    Modified menu template so it works for multi-level menus

--- Resources/views/Menu/item.html.php
+++ Resources/views/Menu/item.html.php
@@ -1,8 +1,8 @@
 <?php if($item->shouldBeRendered()): ?>
     <li <?php echo $view['menu']->attributes($view['menu']->getItemAttributes($item)) ?>>
         <a href="<?php echo $item->getUri() ?>"><?php echo $item->getName() ?></a>
+        <?php if($item->hasChildren()): ?>
+            <?php echo $view->render('KnplabsMenuBundle:Menu:menu.html.php', array('item' => $item)) ?>
+        <?php endif; ?>
     </li>
-    <?php if($item->hasChildren()): ?>
-        <?php echo $view->render('KnplabsMenuBundle:Menu:menu.html.php', array('item' => $item)) ?>
-    <?php endif; ?>
 <?php endif; ?>
