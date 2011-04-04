6bc6debc17f5e67039711e59cef53a2989dd744f
Antoine Hérault <antoine.herault@gmail.com>
2011-04-04 16:28:58 +0200

    Fix rendering of non-link current items

--- Resources/views/Menu/item.html.php
+++ Resources/views/Menu/item.html.php
@@ -1,6 +1,10 @@
 <?php if($item->shouldBeRendered()): ?>
     <li <?php echo $view['menu']->attributes($view['menu']->getItemAttributes($item)) ?>>
+        <?php if (($item->getIsCurrent() && $item->getParent()->getCurrentAsLink()) || !$item->getIsCurrent()): ?>
         <a href="<?php echo $item->getUri() ?>"><?php echo $item->getName() ?></a>
+        <?php else: ?>
+        <span <?php echo $view['menu']->attributes($item->getLabelAttributes()) ?>><?php echo $item->getLabel() ?></span>
+        <?php endif ?>
         <?php if($item->hasChildren()): ?>
             <?php echo $view->render('KnplabsMenuBundle:Menu:menu.html.php', array('item' => $item)) ?>
         <?php endif; ?>
