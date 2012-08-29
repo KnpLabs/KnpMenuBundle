2049cdcbc9714d479a79ff6976c282e386bd3d47
Bilal Amarni <bilal.amarni@gmail.com>
2012-08-29 13:45:54 +0200

    removed test bootstrap file

--- Tests/bootstrap.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-if (!$loader = @include __DIR__ . '/../vendor/autoload.php') {
-    throw new \RuntimeException(
-        'Please install project dependencies before running the test suite'
-    );
-}
