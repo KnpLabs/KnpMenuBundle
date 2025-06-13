bef0003d7a17d2b2fd1a2434eea5901d0fe34f38
Christophe Coevoet <stof@notk.org>
2025-06-13 21:31:57 +0200

    Use a tagged iterator to register menu providers
    The compiler pass is now only responsible for detecting whether we need
    to use the ChainProvider or no, but does not handle the configuration of
    the ChainProvider anymore.

--- tests/DependencyInjection/Compiler/AddProvidersPassTest.php
+++ tests/DependencyInjection/Compiler/AddProvidersPassTest.php
@@ -5,20 +5,10 @@ namespace Knp\Bundle\MenuBundle\Tests\DependencyInjection\Compiler;
 use Knp\Bundle\MenuBundle\DependencyInjection\Compiler\AddProvidersPass;
 use Knp\Menu\Provider\ChainProvider;
 use PHPUnit\Framework\TestCase;
-use Symfony\Component\DependencyInjection\Argument\IteratorArgument;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
-use Symfony\Component\DependencyInjection\Reference;
 
 class AddProvidersPassTest extends TestCase
 {
-    public function testProcessWithoutProviderDefinition(): void
-    {
-        $containerBuilder = new ContainerBuilder();
-        (new AddProvidersPass())->process($containerBuilder);
-
-        self::assertFalse($containerBuilder->hasAlias('knp_menu.menu_provider'));
-    }
-
     public function testProcessForOneProvider(): void
     {
         $containerBuilder = new ContainerBuilder();
@@ -33,12 +23,6 @@ class AddProvidersPassTest extends TestCase
 
     public function testProcessForManyProviders(): void
     {
-        $expectedProviders = [new Reference('id'), new Reference('id2')];
-
-        if (\class_exists(IteratorArgument::class)) {
-            $expectedProviders = new IteratorArgument($expectedProviders);
-        }
-
         $containerBuilder = new ContainerBuilder();
         $containerBuilder->register('knp_menu.menu_provider.chain', ChainProvider::class)->setArgument(0, []);
         $containerBuilder->register('id')->addTag('knp_menu.provider');
@@ -48,9 +32,5 @@ class AddProvidersPassTest extends TestCase
 
         self::assertTrue($containerBuilder->hasAlias('knp_menu.menu_provider'));
         self::assertSame('knp_menu.menu_provider.chain', (string) $containerBuilder->getAlias('knp_menu.menu_provider'));
-
-        self::assertEquals(
-            [$expectedProviders],
-            $containerBuilder->getDefinition('knp_menu.menu_provider.chain')->getArguments());
     }
 }
