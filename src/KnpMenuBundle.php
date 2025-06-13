846d9325ff05df9807b48165af257ae97ecf443a
Christophe Coevoet <stof@notk.org>
2025-06-13 21:22:59 +0200

    Use a tagged locator argument to register renderers
    This brings support for `#[AsTaggedItem]` to define the index.

--- src/KnpMenuBundle.php
+++ src/KnpMenuBundle.php
@@ -4,7 +4,6 @@ namespace Knp\Bundle\MenuBundle;
 
 use Knp\Bundle\MenuBundle\DependencyInjection\Compiler\AddExtensionsPass;
 use Knp\Bundle\MenuBundle\DependencyInjection\Compiler\AddProvidersPass;
-use Knp\Bundle\MenuBundle\DependencyInjection\Compiler\AddRenderersPass;
 use Knp\Bundle\MenuBundle\DependencyInjection\Compiler\RegisterMenusPass;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
@@ -18,7 +17,6 @@ final class KnpMenuBundle extends Bundle
         $container->addCompilerPass(new RegisterMenusPass());
         $container->addCompilerPass(new AddExtensionsPass());
         $container->addCompilerPass(new AddProvidersPass());
-        $container->addCompilerPass(new AddRenderersPass());
     }
 
     public function getPath(): string
